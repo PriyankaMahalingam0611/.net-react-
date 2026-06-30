5024430b41195f75cb708218138127e6f6e468f0
Priyanka M <priyankamaha0611@gmail.com>
2026-06-30 15:36:00 +0530

    Console app with unit testing

---
+++ LibraryMembershipApp/LibraryMembershipApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using LibraryMembershipApp.Interfaces;
+using LibraryMembershipApp.Services;
+
+namespace LibraryMembershipApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Library Membership App!");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("This application is structured to demonstrate NUnit and Moq testing.");
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
